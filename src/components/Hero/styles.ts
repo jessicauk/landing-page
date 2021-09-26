ba856628e0bf3f9b8363228c8a612f7859277515
Jessica Apolinar <jessica_220691@hotmail.com>
2021-09-26 13:59:52 -0500

    deploy

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
+import image from '../../../assets/background.png'
 
 export const StyledHero = styled.div`
   width: 100%;
   height: 100vh;
-  background-image: url(/assets/background.png);
+  background-image: url(${image});
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
